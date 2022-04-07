34a8239d06d7b1fa7c8a26e6782dbd0fa49de6e7
Kop <copter1144@hotmail.com>
2022-04-07 14:11:14 +0700

    keyboard single

---
+++ only_keyboard/menu.py
@@ -0,0 +1,135 @@
+import pygame
+import random
+#import usb
+#from practicum import find_mcu_boards, McuBoard, PeriBoard
+from Tetris import Tetris,Figure
+#from multi import multi_play
+from single import single_play
+
+colors = [
+    (0, 0, 0),
+    (120, 37, 179),
+    (100, 179, 179),
+    (80, 34, 22),
+    (80, 134, 22),
+    (180, 34, 22),
+    (180, 34, 122),
+]
+
+'''
+devs = find_mcu_boards()
+mcu_1 = McuBoard(devs[0])
+peri = PeriBoard(mcu_1)
+if len(devs) == 2 :
+    mcu_2 = McuBoard(devs[1])
+    peri1 = PeriBoard(mcu_2)
+'''
+
+
+# Initialize the game engine
+pygame.init()
+
+size = (800, 500)
+screen = pygame.display.set_mode(size)
+
+menu_state = "menu"
+select = 0
+pygame.display.set_caption("Tetris")
+
+clock = pygame.time.Clock()
+fps = 25
+game = Tetris(20, 10)
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
+YELLOW = (255, 255, 0)
+
+#multi_play(peri, peri1, colors, screen)
+#single_play(peri, colors, screen)
+done = False
+check_up = 1
+check_left = 1
+check_right = 1
+check_down = 1
+
+while not done:
+    screen.fill(BLACK)
+    
+
+    font = pygame.font.SysFont('Calibri', 25, True, False)
+    text = font.render("Single Player", True, WHITE)
+    text2 = font.render("Multi Player", True, WHITE)
+    screen.blit(text, [200,100 ])
+    screen.blit(text2, [200,150])
+    select = select%2
+    pygame.draw.rect(screen, WHITE,
+                     [180,
+                      100+select*50,
+                      20, 20])
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and check_up:
+                pygame.draw.rect(screen, BLACK, [180, 100 + select * 50, 20, 20])
+                select -= 1
+                check_up = 0
+            if event.key == pygame.K_DOWN and check_down:
+                pygame.draw.rect(screen, BLACK, [180, 100 + select * 50, 20, 20])
+                select += 1
+                check_down = 0
+            if event.key == pygame.K_RIGHT and check_right:
+                if select == 0:
+                    single_play(colors, screen)
+                else:
+                    multi_play(colors, screen)
+                check_right = 0
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                check_up = 1
+            if event.key == pygame.K_DOWN:
+                check_down = 1
+            if event.key == pygame.K_RIGHT:
+                check_right = 1
+        '''
+        if not peri.get_up():
+            check_up = 1
+        if not peri.get_left():
+            check_left = 1
+        if not peri.get_right():
+            check_right = 1
+        if not peri.get_down():
+            check_down = 1
+
+        
+    if peri.get_up() and check_up:
+        pygame.draw.rect(screen, BLACK, [180, 100 + select * 50, 20, 20])
+        select -= 1
+        check_up = 0
+    if peri.get_down() and check_down:
+        pygame.draw.rect(screen, BLACK, [180, 100 + select * 50, 20, 20])
+        select += 1
+        check_down = 0
+    if peri.get_right() and check_right:
+        if select == 0:
+            single_play(peri, colors, screen)
+        else:
+            multi_play(peri, peri1, colors, screen)
+        check_right = 0
+
+    if not peri.get_up():
+        check_up = 1
+    if not peri.get_left():
+        check_left = 1
+    if not peri.get_right():
+        check_right = 1
+    if not peri.get_down():
+        check_down = 1
+    '''
+    pygame.display.flip()
+    clock.tick(fps)
+
+pygame.quit()
---
+++ only_keyboard/single.py
@@ -0,0 +1,205 @@
+import pygame
+import random
+#import usb
+#from practicum import find_mcu_boards, McuBoard, PeriBoard
+from Tetris import Tetris,Figure
+
+def single_play(colors, screen):
+    # Define some colors
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GRAY = (128, 128, 128)
+    YELLOW = (255, 255, 0)
+
+    # Loop until the user clicks the close button.
+    done = False
+    clock = pygame.time.Clock()
+    fps = 25
+    game = Tetris(20, 10)
+    counter = 0
+    game.x += 200
+    game.next_blockX += 200
+
+    pressing_down = False
+
+    #light_min = peri.get_light()
+
+    check_up = 1
+    check_left = 1
+    check_right = 1
+    game.state = "start"
+    while not done:
+        '''
+        if game.state == "calibrate":
+            if peri.get_down():
+                light_min = peri.get_light()
+                game.state = "start"
+        '''
+        
+        if game.figure is None:
+            game.figure = Figure(3, 0)
+            game.next_figure = Figure(3, 0)
+        counter += 1
+        if counter > 100000:
+            counter = 0
+
+        if counter % (fps // game.level // 2) == 0 or pressing_down:
+            if game.state == "start" or game.state == "obstacle":
+                game.go_down()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN and game.state != "game over":
+                if event.key == pygame.K_UP:
+                    game.rotate()
+                if event.key == pygame.K_DOWN:
+                    pressing_down = True
+                if event.key == pygame.K_LEFT:
+                    game.go_side(-1)
+                if event.key == pygame.K_RIGHT:
+                    game.go_side(1)
+                if event.key == pygame.K_SPACE:
+                    game.go_space()
+                if event.key == pygame.K_ESCAPE:
+                    game.__init__(20, 10)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN:
+                    pressing_down = False
+        '''
+        if game.state == "start" or game.state == "obstacle":
+            if peri.get_light() >= 0.5 * light_min:
+                if peri.get_up() and check_up:
+                    game.rotate()
+                    check_up = 0
+                if peri.get_down():
+                    pressing_down = True
+                if peri.get_left() and check_left:
+                    game.go_side(-1)
+                    check_left = 0
+                if peri.get_right() and check_right:
+                    game.go_side(1)
+                    check_right = 0
+
+                if not peri.get_up():
+                    check_up = 1
+                if not peri.get_left():
+                    check_left = 1
+                if not peri.get_right():
+                    check_right = 1
+
+                if not peri.get_down():
+                    pressing_down = False
+        '''
+        if game.state == "game over":
+            '''
+            if peri.get_right() :
+                game.__init__(20, 10)
+
+            if peri.get_left() :
+                return
+            '''
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    game.__init__(20, 10)
+                if event.key == pygame.K_LEFT:
+                    return
+
+        screen.fill(WHITE)
+        
+        if game.state == "obstacle":
+            a = peri.get_light()
+            if a <= 0.5 * light_min:
+                game.state = "start"
+                print(a, light_min)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+
+            for i in range(game.height):
+                for j in range(game.width):
+                    pygame.draw.rect(screen, GRAY,
+                                     [game.x + game.zoom * j, game.y + game.zoom * i, game.zoom, game.zoom], 1)
+                    pygame.draw.rect(screen, YELLOW,
+                                     [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2,
+                                      game.zoom - 1])
+
+            if game.figure is not None:
+                for i in range(4):
+                    for j in range(4):
+                        p = i * 4 + j
+                        if p in game.figure.image():
+                            pygame.draw.rect(screen, colors[game.figure.color],
+                                             [game.x + game.zoom * (j + game.figure.x) + 1,
+                                              game.y + game.zoom * (i + game.figure.y) + 1,
+                                              game.zoom - 2, game.zoom - 2])
+            if game.next_figure is not None:
+                for i in range(4):
+                    for j in range(4):
+                        pygame.draw.rect(screen, GRAY,
+                                         [game.next_blockX + game.zoom * (j),
+                                          game.next_blockY + game.zoom * (i), game.zoom, game.zoom], 1)
+                        p = i * 4 + j
+                        if p in game.next_figure.image():
+                            pygame.draw.rect(screen, colors[game.next_figure.color],
+                                             [game.next_blockX + game.zoom * (j) + 1,
+                                              game.next_blockY + game.zoom * (i) + 1,
+                                              game.zoom - 2, game.zoom - 2])
+        else:
+            for i in range(game.height):
+                for j in range(game.width):
+                    pygame.draw.rect(screen, GRAY,
+                                     [game.x + game.zoom * j, game.y + game.zoom * i, game.zoom, game.zoom], 1)
+                    if game.field[i][j] > 0:
+                        pygame.draw.rect(screen, colors[game.field[i][j]],
+                                         [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2,
+                                          game.zoom - 1])
+
+            if game.figure is not None:
+                for i in range(4):
+                    for j in range(4):
+                        p = i * 4 + j
+                        if p in game.figure.image():
+                            pygame.draw.rect(screen, colors[game.figure.color],
+                                             [game.x + game.zoom * (j + game.figure.x) + 1,
+                                              game.y + game.zoom * (i + game.figure.y) + 1,
+                                              game.zoom - 2, game.zoom - 2])
+
+            if game.next_figure is not None:
+                for i in range(4):
+                    for j in range(4):
+                        pygame.draw.rect(screen, GRAY,
+                                         [game.next_blockX + game.zoom * (j),
+                                          game.next_blockY + game.zoom * (i), game.zoom, game.zoom], 1)
+                        p = i * 4 + j
+                        if p in game.next_figure.image():
+                            pygame.draw.rect(screen, colors[game.next_figure.color],
+                                             [game.next_blockX + game.zoom * (j) + 1,
+                                              game.next_blockY + game.zoom * (i) + 1,
+                                              game.zoom - 2, game.zoom - 2])
+
+        font_socre = pygame.font.SysFont('Calibri', 32, True, False)
+        font_next = pygame.font.SysFont('Calibri', 14, True, False)
+        font_game_over = pygame.font.SysFont('Calibri',72 , True, False)
+        font_press = pygame.font.SysFont('Calibri',44 , True, False)
+
+        text = font_socre.render("Score: " + str(game.score), True, BLACK)
+        text_game_over = font_game_over.render("Game Over", True, (255, 125, 0))
+        text_press_right = font_press.render("Press right : Play again", True, (255, 215, 0))
+        text_press_left = font_press.render("Press left   : Return to menu", True, (255, 215, 0))
+        text_next = font_next.render("NEXT BLOCK", True, (0, 0, 0))
+
+        screen.blit(text, [0, 0])
+        screen.blit(text_next, [game.next_blockX , 180])
+        if game.state == "game over":
+            screen.blit(text_game_over, [220, 150])
+            screen.blit(text_press_right, [140, 250])
+            screen.blit(text_press_left, [140, 300])
+
+        pygame.display.flip()
+        clock.tick(fps)
+        #print(game.state)
+
+    pygame.quit()
